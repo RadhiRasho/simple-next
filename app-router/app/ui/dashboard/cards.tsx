bf6dd824945ea7e988fa7a274eba6c894e0a3636
Radhi Rasho <radhi.h@outlook.com>
2025-08-24 19:09:28 -0500

    refactor: simplify dashboard page by integrating skeletons and fetching data within components

--- app-router/app/ui/dashboard/cards.tsx
+++ app-router/app/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
 	InboxIcon,
 	UserGroupIcon,
 } from "@heroicons/react/24/outline";
+import { fetchCardData } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
@@ -14,31 +15,34 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+	const {
+		numberOfCustomers,
+		numberOfInvoices,
+		totalPaidInvoices,
+		totalPendingInvoices,
+	} = await fetchCardData();
+
 	return (
 		<>
-			{/* NOTE: Uncomment this code in Chapter 9 */}
-
-			{/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+			<Card title="Collected" value={totalPaidInvoices} type="collected" />
+			<Card title="Pending" value={totalPendingInvoices} type="pending" />
+			<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+			<Card title="Total Customers" value={numberOfCustomers} type="customers" />
 		</>
 	);
+}
+
+type CardProps = {
+	title: string;
+	value: number | string;
+	type: "invoices" | "customers" | "pending" | "collected";
 }
 
 export function Card({
 	title,
 	value,
 	type,
-}: {
-	title: string;
-	value: number | string;
-	type: "invoices" | "customers" | "pending" | "collected";
-}) {
+}: CardProps) {
 	const Icon = iconMap[type];
 
 	return (
